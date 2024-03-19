f365c7fbebf811e640154de0db10ef54626543b1
Robin Manhaeve <robin.manhaeve@cs.kuleuven.be>
2024-03-19 13:16:05 +0100

    Deleted old files

--- mapnik/draw_nik4.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#nik4 -v -s 25000 --ppi 300 -a 4 -c 673689.467321 674925.873282 --margin 10 -P "$(<transformation.proj4)" opentopomap.xml 4print.png
-nik4 -v -s 25000 --ppi 300 -a 4 -c 4.675884 50.902600 --margin 10 -P "$(<transformation.proj4)" opentopomap.xml 4print.png
-
